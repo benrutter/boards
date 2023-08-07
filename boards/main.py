4558940879e83c9569f545379eeae256b402661e
Ben <ben@02819>
2023-08-07 17:03:28 +0100

    promote functionality

--- boards/main.py
+++ boards/main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 import pathlib
+import shutil
 
 from rich import print, table
 import typer
@@ -6,10 +8,13 @@ import tomlkit
 
 
 class BoardsApp:
-    def __init__(self, board="default", promote=None, demote=None):
+    def __init__(self, board="default", promote="", demote=""):
         self.user_config = self.get_user_config()
-        self.board_dir = self.user_config["boards"][board]
+        self.board_dir = pathlib.Path(self.user_config["boards"][board])
         self.board_config = self.get_board_config()
+        self.lanes = self.board_config["lanes"]
+        if promote:
+            self.promote(promote)
         self.board = self.get_board()
         self.display_board()
 
@@ -57,5 +62,29 @@ class BoardsApp:
             board_table.add_row(*[display_board[k][i] for k in display_board])
         print(board_table)
 
+    def promote(self, item):
+        """
+        promote selected item to new location
+        steps:
+        1. find item
+        2. move item
+        """
+        lane_dirs: List[Path] = [self.board_dir / pathlib.Path(i) for i in self.lanes]
+        found: Optional[Path] = None
+        for lane in lane_dirs:
+            items: List[Path] = list(lane.iterdir())
+            matches = [i for i in items if i.stem == item]
+            if len(matches) > 0:
+                found: Path = matches[0]
+                found_in: Path = lane
+                break
+        if not found:
+            print(f"[red]Could not find {item} in board[/red]")
+            return
+        move_to = self.lanes.index(found_in.stem) + 1
+        if move_to >= len(self.lanes):
+            print(f"[red]Cannot promote {item} past {found_in.stem}[red]")
+            return
+        shutil.move(found, lane_dirs[move_to] / (found.stem + found.suffix))
 
 typer.run(BoardsApp)
